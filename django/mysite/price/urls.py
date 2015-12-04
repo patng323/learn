d945ecc64a185a82fba17cf59609d9715e1d0dfa
Patrick Ng <patng323@gmail.com>
2015-12-04 09:24:12 +0800

    Minor update

--- django/mysite/price/urls.py
+++ django/mysite/price/urls.py
@@ -5,10 +5,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/$', views.search, name='search'),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
