ad094b7ff30c3b9c7f8182149b40829746be28ce
Patrick Ng <patng323@gmail.com>
2015-12-31 16:46:53 +0800

    Brand and price level boost Use 0.0.0.0 for runserver

--- django/mysite/price/__init__.py
+++ django/mysite/price/__init__.py
@@ -8,8 +8,8 @@ def initProductInfo():
     imgBase = "http://www.price.com.hk/space/product/"
     if productDf is None:
         print "productDf inited"
-        productDf = pd.read_csv(dataPath,
-                names=['id', 'brand', 'name', 'c0_id', 'c0', 'c1_id', 'c1', 'c2_id', 'c2', 'priceType', 'price', 'img' ])
+        productDf = pd.read_csv(dataPath, sep = '\t',
+                names=['id', 'brand', 'name', 'c0_id', 'c0', 'c1_id', 'c1', 'c2_id', 'c2', 'priceType', 'price', 'img', 'priceLevel' ])
         productDf.set_index('id', inplace=True)
         productDf.ix[:,'img'] = imgBase + productDf.ix[:, 'img']
 
